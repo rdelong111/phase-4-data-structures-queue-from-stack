4fd4b9ccfff89a4e7e2fd72e856ba033a7988560
Ryan DeLong <rdelong111@gmail.com>
2022-12-31 18:32:32 -0600

    commit tree 8b7d0f871b450e2397bac2ffb783ab2618668f39

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,38 @@
 require_relative './stack'
 
 # your code here
+class MyQueue
+  def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+  end
+
+  def add(value)
+    back_order
+    @s1.push(value)
+  end
+
+  def remove
+    right_order
+    @s2.pop
+  end
+
+  def peek
+    right_order
+    @s2.peek
+  end
+
+  def right_order
+    until @s1.empty?
+      @s2.push(@s1.pop)
+    end
+    @s2
+  end
+
+  def back_order
+    until @s2.empty?
+      @s1.push(@s2.pop)
+    end
+    @s1
+  end
+end
